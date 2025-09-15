a0fa72189048a0c839c7ad1424f3d982934ed6c5
nEspinozaTroncoso <nicolasespinoza1985@gmail.com>
2025-09-15 14:54:42 -0300

    Cap 38 terminado!!

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+// PNG
+export { default as logoGreen } from "./png/logo-green.png";
+export { default as logoNameGreen } from "./png/logo-name-green.png";
+export { default as LogoNameWhite } from "./png/logo-name-white.png";
+export { default as logoWhite } from "./png/logo-white.png";
+export { default as noImage } from "./png/no-image.png";
+export { default as defaultUser } from "./png/default-user.png";
+
+// JPG
+export { default as backgroundAuth } from "./jpg/background-auth.jpg";
+export { default as bannerHome } from "./jpg/banner-home.jpg";
