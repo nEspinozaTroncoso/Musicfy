24d8f584193548688e01ae36e760c3da90059a10
nEspinozaTroncoso <nicolasespinoza1985@gmail.com>
2025-09-15 13:19:37 -0300

    Cap 36 terminado!!

--- src/routes/LoggedNavigation.js
+++ src/routes/LoggedNavigation.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, Artists, Artist, Albums, Album, Profile } from "../pages";
 
 export function LoggedNavigation() {
   return (
-    <div>
-      <Button secondary>LoggedNavigation</Button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/artists/:id" element={<Artist />} />
+        <Route path="/albums" element={<Albums />} />
+        <Route path="/albums/:id" element={<Album />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
