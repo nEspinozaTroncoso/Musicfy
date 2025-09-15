9f9bebe61f045135aeca0a15122a23d8623b70d3
nEspinozaTroncoso <nicolasespinoza1985@gmail.com>
2025-09-15 16:01:54 -0300

    Cap 42 terminado!!

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,18 +1,46 @@
 import React from "react";
-import { Button } from "semantic-ui-react";
+import { Form, Icon } from "semantic-ui-react";
+import "./RegisterForm.scss";
 
 export function RegisterForm(props) {
   const { openLogin, goBack } = props;
   return (
-    <div style={{ backgroundColor: "#f00" }}>
-      <h1>RegisterForm</h1>
+    <div className="register-form">
+      <h1>Empieza a escuchar con una cuenta de Musicfy gratis.</h1>
 
-      <Button primary onClick={openLogin}>
-        Login
-      </Button>
-      <Button secondary onClick={goBack}>
-        Atras
-      </Button>
+      <Form>
+        <Form.Input
+          type="text"
+          placeholder="Correo electronico"
+          icon="mail outline"
+        />
+        <Form.Input
+          type="password"
+          placeholder="Contraseña"
+          icon={
+            <Icon
+              name="eye"
+              link
+              onClick={() => console.log("Show Password")}
+            />
+          }
+        />
+        <Form.Input
+          type="text"
+          placeholder="¿Como deberíamos llamarte"
+          icon="user circle outline"
+        />
+
+        <Form.Button type="submit" primary fluid>
+          Continuar
+        </Form.Button>
+      </Form>
+      <div className="register-form__options">
+        <p onClick={goBack}>Volver</p>
+        <p>
+          ¿Ya tienes Musicfy? <span onClick={openLogin}>Iniciar sesión</span>
+        </p>
+      </div>
     </div>
   );
 }
