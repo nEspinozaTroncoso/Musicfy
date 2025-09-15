7ed6506a9cfa7313199a1efd5f4e6d6104af5ec3
nEspinozaTroncoso <nicolasespinoza1985@gmail.com>
2025-09-15 15:20:53 -0300

    Cap 40 terminado!!

--- src/components/Auth/AuthOptions/AuthOptions.js
+++ src/components/Auth/AuthOptions/AuthOptions.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Button } from "semantic-ui-react";
+import "./AuthOptions.scss";
 
 export function AuthOptions(props) {
   const { openLogin, openRegister } = props;
   return (
-    <div style={{ backgroundColor: "#000" }}>
-      <h1>AuthOptions</h1>
+    <div className="auth-options">
+      <h1>Millones de canciones, gratis en Musicfy</h1>
 
-      <Button primary onClick={openRegister}>
-        Registro
+      <Button className="register" onClick={openRegister}>
+        Registrate gratis
       </Button>
-      <Button secondary onClick={openLogin}>
-        Login
+      <Button className="login" onClick={openLogin}>
+        Inicia sesión
       </Button>
     </div>
   );
