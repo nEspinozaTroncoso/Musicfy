df352f1573203621bd8e464b5c82d7b896bf2e55
nEspinozaTroncoso <nicolasespinoza1985@gmail.com>
2025-09-16 12:11:13 -0300

    Cap 48 terminado!!

--- src/components/Auth/LoginForm/LoginForm.js
+++ src/components/Auth/LoginForm/LoginForm.js
@@ -1,18 +1,46 @@
-import React from "react";
-import { Button } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Form, Icon } from "semantic-ui-react";
+import "./LoginForm.scss";
 
 export function LoginForm(props) {
   const { openRegister, goBack } = props;
+  const [showPassword, setShowPassword] = useState(false);
+
+  const onShowHiddenPassword = () => setShowPassword((prevState) => !prevState);
 
   return (
-    <div style={{ backgroundColor: "#f0f" }}>
-      <h1>LoginForm</h1>
-      <Button primary onClick={openRegister}>
-        Registro
-      </Button>
-      <Button secondary onClick={goBack}>
-        Atras
-      </Button>
+    <div className="login-form">
+      <h1>Musica para todos</h1>
+      <Form>
+        <Form.Input
+          name="email"
+          type="text"
+          placeholder="Correo electronico"
+          icon="mail outline"
+        />
+        <Form.Input
+          name="password"
+          type={showPassword ? "text" : "password"}
+          placeholder="Contraseña"
+          icon={
+            <Icon
+              name={showPassword ? "eye slash" : "eye"}
+              link
+              onClick={onShowHiddenPassword}
+            />
+          }
+        />
+        <Form.Button type="submit" primary fluid>
+          Inicia Sesión
+        </Form.Button>
+      </Form>
+
+      <div className="login-form__options">
+        <p onClick={goBack}>Volver</p>
+        <p on>
+          ¿No tienes cuenta? <span onClick={openRegister}>Registrarse</span>
+        </p>
+      </div>
     </div>
   );
 }
