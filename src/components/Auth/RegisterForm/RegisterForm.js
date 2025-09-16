b9f5914dc61a3878ece1ee09e969403ba7f809e4
nEspinozaTroncoso <nicolasespinoza1985@gmail.com>
2025-09-16 10:36:48 -0300

    Cap  44 terminado!!

--- src/components/Auth/RegisterForm/RegisterForm.js
+++ src/components/Auth/RegisterForm/RegisterForm.js
@@ -1,20 +1,35 @@
 import React from "react";
 import { Form, Icon } from "semantic-ui-react";
+import { useFormik } from "formik";
+import { initialValues } from "./RegisterForm.data";
 import "./RegisterForm.scss";
 
 export function RegisterForm(props) {
   const { openLogin, goBack } = props;
+
+  const formik = useFormik({
+    initialValues: initialValues(),
+    onSubmit: (formValue) => {
+      console.log("Registro OK");
+      console.log(formValue);
+    },
+  });
+
   return (
     <div className="register-form">
       <h1>Empieza a escuchar con una cuenta de Musicfy gratis.</h1>
 
-      <Form>
+      <Form onSubmit={formik.handleSubmit}>
         <Form.Input
+          name="email"
           type="text"
           placeholder="Correo electronico"
           icon="mail outline"
+          onChange={formik.handleChange}
+          value={formik.values.email}
         />
         <Form.Input
+          name="password"
           type="password"
           placeholder="Contraseña"
           icon={
@@ -24,11 +39,16 @@ export function RegisterForm(props) {
               onClick={() => console.log("Show Password")}
             />
           }
+          onChange={formik.handleChange}
+          value={formik.values.password}
         />
         <Form.Input
+          name="username"
           type="text"
           placeholder="¿Como deberíamos llamarte"
           icon="user circle outline"
+          onChange={formik.handleChange}
+          value={formik.values.username}
         />
 
         <Form.Button type="submit" primary fluid>
