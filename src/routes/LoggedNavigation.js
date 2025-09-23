96b5b29ea593182c7830cdd13ab8b483f214a071
nEspinozaTroncoso <nicolasespinoza1985@gmail.com>
2025-09-23 11:52:12 -0300

    Cap 51 terminado!!

--- src/routes/LoggedNavigation.js
+++ src/routes/LoggedNavigation.js
@@ -1,18 +1,21 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Artists, Artist, Albums, Album, Profile } from "../pages";
+import { LoggedLayout } from "../layouts";
 
 export function LoggedNavigation() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/artists" element={<Artists />} />
-        <Route path="/artists/:id" element={<Artist />} />
-        <Route path="/albums" element={<Albums />} />
-        <Route path="/albums/:id" element={<Album />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
+      <LoggedLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/artists" element={<Artists />} />
+          <Route path="/artists/:id" element={<Artist />} />
+          <Route path="/albums" element={<Albums />} />
+          <Route path="/albums/:id" element={<Album />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </LoggedLayout>
     </BrowserRouter>
   );
 }
